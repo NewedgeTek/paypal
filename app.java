c94561954d636e892e8e4737742422297f287050
fritzo21 <126033758+fritzo21@users.noreply.github.com>
2023-02-26 14:58:25 -0600

    Update app.java

--- app.java
+++ app.java
@@ -1,1 +1,18 @@
 This is great
+#This cript is to authenticate library users nbefore they can sign books out
+#!/bin/bash
+ library=Menomenee-public-library
+echo "Please enter you name"
+read name
+echo "Welcome $name to $library
+echo "Today the"
+date
+echo "can you please enter your password for authentication"
+read -s password
+if [ $password == fritzo21 ]
+then
+echo "Thank you!! You were authenticated"
+echo "you can go ahead and enter the title of book/s you want to borrow"
+else
+echo "sorry you entered the wrong password"
+echo "See the the front desk for more assistance"
